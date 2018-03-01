a5da3976b6449ff9b068943765e63b7f7f2356ec
Cathy Siller <cathysiller503@gmail.com>
2018-03-01 11:52:28 -0600

    chore(wip): vue demo portion

--- docs/components/choice-tiles/choice-tile-demo.js
+++ docs/components/choice-tiles/choice-tile-demo.js
@@ -1,17 +1,52 @@
-if (document.getElementById('vue-choiceTileDemo')) {
+import Util from '../../_util';
+
+if (document.getElementById('vue-choiceDemo')) {
     new Vue({
-        el: '#vue-choiceTileDemo',
+        el: '#vue-choiceDemo',
         data: {
-            isHovered: false,
-            variant: {
+            size: {
                 label: 'Default',
                 value: '',
             },
-            variants: [
+            sizes: [
+                { value: 'hxSm', label: 'Small' },
                 { value: '', label: 'Default' },
-                { value: 'checked', label: 'Selected' },
-                { value: 'invalid', label: 'Invalid' },
+                { value: 'hxLg', label: 'Large' },
             ],
+            description: 'A couple of descriptive lines or a small bit of help text.',
+            isChecked: false,
+            isDisabled: false,
+            isInvalid: false,
+            isSubdued: false,
+        },
+        computed: {
+            tileClasses: function () {
+                let out = [];
+                out.push(this.size.value);
+                if (this.isSubdued) {
+                    out.push('hxSubdued');
+                }
+                return out.join(' ').trim();
+            },
+            hasClasses: function () {
+                return (this.tileClasses !== '');
+            },
+            snippet: function () {
+                return Util.snippet(`
+                  <label class="hxChoice">
+                    <input 
+                        type="radio" 
+                        ${this.isChecked ? 'checked' : ''}
+                        ${this.isDisabled ? 'disabled' : ''}
+                        ${this.isInvalid ? 'invalid' : ''}>
+                    <hx-tile ${this.hasClasses ? `class="${this.tileClasses}"` : ''}>
+                        <p class="hxSubbody"> 
+                          ${this.description}
+                        </p>
+                    </hx-tile>
+                  </label>
+                `);
+            },
         },
     });
 }
